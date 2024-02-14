ea2e1ab3e22e968dde8e435007e9ba32504a5579
Mello Birkan <mgbirkan@icloud.com>
2024-02-14 16:04:28 -0300

    Foi adicionado a função para salvar os dados, apresentar esses dados na tela do usuario e deletar eles.

--- VoyageMate/ContentView.swift
+++ VoyageMate/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-  @Query var destination: [Destination]
+  @Environment(\.modelContext) var modelContext
+  @Query var destinations: [Destination]
   
   var body: some View {
-    Text("Hello")
+    NavigationStack {
+      List {
+        ForEach(destinations) { destination in
+          VStack(alignment: .leading) {
+            Text(destination.name)
+              .font(.headline)
+            
+            Text(destination.date.formatted(date: .long, time: .shortened))
+          }
+        }
+        .onDelete(perform: deleteDestination)
+      }
+      .navigationTitle("Voyage Mate")
+      .toolbar {
+        Button("Add Samples", action: addSamples)
+      }
+    }
+  }
+  
+  func addSamples() {
+    let rome = Destination(name: "Rome")
+    let florence = Destination(name: "Florence")
+    let naples = Destination(name: "Naples")
+    
+    modelContext.insert(rome)
+    modelContext.insert(florence)
+    modelContext.insert(naples)
+  }
+  
+  func deleteDestination(_ indexSet: IndexSet) {
+    for index in indexSet {
+      let destination = destinations[index]
+      modelContext.delete(destination)
+    }
   }
 }
-  
-  #Preview {
-    ContentView()
-  }
+
+#Preview {
+  ContentView()
+}
