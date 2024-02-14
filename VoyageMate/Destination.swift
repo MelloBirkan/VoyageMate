7fce45b73104eb8216b80d6511c5ce7fbd765813
Mello Birkan <mgbirkan@icloud.com>
2024-02-14 15:48:36 -0300

    BoilerPlate swiftData.

---
+++ VoyageMate/Destination.swift
@@ -0,0 +1,25 @@
+//
+//  Item.swift
+//  VoyageMate
+//
+//  Created by Marcello Gonzatto Birkan on 14/02/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Destination {
+  var name: String
+  var details: String
+  var date: Date
+  var priority: Int
+  
+  init(name: String = "", details: String = "", date: Date = .now, priority: Int = 2) {
+    self.name = name
+    self.details = details
+    self.date = date
+    self.priority = priority
+  }
+}
+
